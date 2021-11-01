36a3b9de68466fbbdba305686af8063ae180c08d
parker9706 <pdhutcheson@gmail.com>
2021-11-01 14:13:05 -0700

    "Configuring webpack, many errors
    Co-authored-by: Angelynn Truong <angelynn.trng@gmail.com>"

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // Where files should be sent once they are bundled
+  entry: {
+    index: './src/index.tsx',
+  },
  output: {
    path: path.join(__dirname, '/dist'),
    filename: 'index.bundle.js'
@@ -14,13 +17,13 @@ module.exports = {
   // Rules of how webpack will take our files, complie & bundle them for the browser
  module: {
    rules: [
-     {
-       test: /\.(js|jsx)$/,
+    {
+       test: /\.(ts|tsx)$/,
        exclude: /nodeModules/,
        use: {
          loader: 'babel-loader'
        }
-     },
+    },
      {
        test: /\.css$/,
        use: ['style-loader', 'css-loader']
