127baa2e3f36e3b0c5d0086d4a3b6d9d2aabf6d9
parker9706 <pdhutcheson@gmail.com>
2021-11-01 09:45:29 -0700

    "Modified pages
    Co-authored-by: Angelynn Truong <angelynn.trng@gmail.com>"

---
+++ product-page/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+
+import Credits from './Credits';
+
+export default function Homepage{
+  return (<>
+    <Stack>
+      <Credits />
+    </Stack>
+  </>)
+}
