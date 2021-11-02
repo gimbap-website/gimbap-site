4a98e732818b55f3da30e5df397cc2e586022583
parker9706 <pdhutcheson@gmail.com>
2021-11-01 17:26:17 -0700

    "Changes to app slightly were made in order to create a div that would be linkable in CSS and remove margins
    Co-authored-by: Angelynn Truong <angelynn.trng@gmail.com>"

--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -5,6 +5,7 @@ import Button from '@mui/material/Button';
 export default function Navbar() {
   return (
     <div className="dark nav-bar">
+
       <Button variant="text">Demo</Button>
       <Button variant="text">Documentation</Button>
     </div>
