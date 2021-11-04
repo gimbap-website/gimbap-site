039ce2f20e422d1df048d7aee3110a82c11361f9
parker9706 <pdhutcheson@gmail.com>
2021-11-04 15:46:57 -0700

    Adding in Credits page

--- src/product-page/Credits.tsx
+++ src/product-page/Credits.tsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import Link from '@mui/material/Link';
+import Link from '@material-ui/core/Link';
 import Stack from '@mui/material/Stack';
 
 
@@ -12,14 +12,51 @@ import Stack from '@mui/material/Stack';
 
 export default function Credits() {
   return (
-  <div className="dark">
-    <Link href="https://github.com/oslabs-beta/gimbap">
+    <div className="dark">
+    <Typography variant="h2" component="h2" id="credits" color="textPrimary">the gimbap team</Typography>
+    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>MH</Avatar>
+    <Typography variant="body1" id="credits-body1" color="textPrimary">Miguel Hernandez</Typography>
+    <Link href="https://github.com/miguelh72">
     <GitHubIcon />
     </Link>
-    <Link href="https://www.linkedin.com">
+    <Link href="https://www.linkedin.com/in/miguelh72/">
     <LinkedInIcon />
     </Link>
 
-    {/* 24px medium article logo */}
+    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>AT</Avatar>
+    <Typography variant="body1" id="credits-body2" color="textPrimary">Angelynn Truong</Typography>
+    <Link href="https://github.com/vngelynn">
+    <GitHubIcon />
+    </Link>
+    <Link href="https://www.linkedin.com/in/vngelynn/">
+    <LinkedInIcon />
+    </Link>
+
+    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>SF</Avatar>
+    <Typography variant="body1" id="credits-body3" color="textPrimary">Sebastien Fauque</Typography>
+    <Link href="https://github.com/SebastienFauque">
+    <GitHubIcon />
+    </Link>
+    <Link href="https://www.linkedin.com/in/sebastienfauque/">
+    <LinkedInIcon />
+    </Link>
+
+    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>KI</Avatar>
+    <Typography variant="body1" id="credits-body4" color="textPrimary">Khandker Islam</Typography>
+    <Link href="https://github.com/khandkerislam">
+    <GitHubIcon />
+    </Link>
+    <Link href="https://www.linkedin.com/in/khandkerislam/">
+    <LinkedInIcon />
+    </Link>
+
+    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>PH</Avatar>
+    <Typography variant="body1" id="credits-body5" color="textPrimary">Parker Hutcheson</Typography>
+    <Link href="https://github.com/Parker9706">
+    <GitHubIcon />
+    </Link>
+    <Link href="https://www.linkedin.com/in/parkerhutcheson/">
+    <LinkedInIcon />
+    </Link>
   </div> );
 }
