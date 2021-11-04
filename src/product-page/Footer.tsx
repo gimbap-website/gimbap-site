bb2c6c998e2786a8f7ccecca6ca4cf43363fa195
parker9706 <pdhutcheson@gmail.com>
2021-11-04 14:27:30 -0700

    Message

--- src/product-page/Footer.tsx
+++ src/product-page/Footer.tsx
@@ -13,11 +13,10 @@ import Stack from '@mui/material/Stack';
 export default function Credits() {
   return (
   <div className="dark">
-    <Typography variant="h2" component="h2" id="credits" color="textPrimary">the gimbap team</Typography>
+
 
     <Stack direction="row" spacing={2}>
-    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>MH</Avatar>
-    <Typography variant="body1" id="credits-body1" color="textPrimary">Miguel Hernandez</Typography>
+
     <Link href="https://github.com/miguelh72">
     <GitHubIcon />
     </Link>
@@ -25,41 +24,7 @@ export default function Credits() {
     <LinkedInIcon />
     </Link>
 
-    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>AT</Avatar>
-    <Typography variant="body1" id="credits-body2" color="textPrimary">Angelynn Truong</Typography>
-    <Link href="https://github.com/vngelynn">
-    <GitHubIcon />
-    </Link>
-    <Link href="https://www.linkedin.com/in/vngelynn/">
-    <LinkedInIcon />
-    </Link>
+</Stack>
 
-    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>SF</Avatar>
-    <Typography variant="body1" id="credits-body3" color="textPrimary">Sebastien Fauque</Typography>
-    <Link href="https://github.com/SebastienFauque">
-    <GitHubIcon />
-    </Link>
-    <Link href="https://www.linkedin.com/in/sebastienfauque/">
-    <LinkedInIcon />
-    </Link>
-
-    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>KI</Avatar>
-    <Typography variant="body1" id="credits-body4" color="textPrimary">Khandker Islam</Typography>
-    <Link href="https://github.com/khandkerislam">
-    <GitHubIcon />
-    </Link>
-    <Link href="https://www.linkedin.com/in/khandkerislam/">
-    <LinkedInIcon />
-    </Link>
-
-    <Avatar sx={{ bgcolor: "#28A78D", width: 100, height: 100 }}>PH</Avatar>
-    <Typography variant="body1" id="credits-body5" color="textPrimary">Parker Hutcheson</Typography>
-    <Link href="https://github.com/Parker9706">
-    <GitHubIcon />
-    </Link>
-    <Link href="https://www.linkedin.com/in/parkerhutcheson/">
-    <LinkedInIcon />
-    </Link>
-    </Stack>
   </div> );
 }
