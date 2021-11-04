10846a048d2e8cacb714344a95a22130c09e2d92
parker9706 <pdhutcheson@gmail.com>
2021-11-04 09:33:58 -0700

    Coding divider for Gimbap website

--- src/product-page/Divider.tsx
+++ src/product-page/Divider.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
 
 export default function Divider() {
   return (
   <div className="dark">
- <p> this is the divider </p>
+ <Typography variant="h5" className="feature-header" id="feature-header-1">Cluster Reccomendations</Typography>
+        <Typography variant="body1" className="feature-body" id="feature-body-1">Provide detailed recomendations on where your application could benefit from a move to a microservice architecture.</Typography>
   </div> );
 }
