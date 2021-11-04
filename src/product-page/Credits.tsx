e6b9f62410a327550d05302d5d47e61810437646
parker9706 <pdhutcheson@gmail.com>
2021-11-04 13:53:48 -0700

    Added core imports for the Credits page

--- src/product-page/Credits.tsx
+++ src/product-page/Credits.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Link from '@material-ui/core/Link'; //need to fix
+
+
+
+
 
 export default function Credits() {
   return (
   <div className="dark">
-    <Typography variant="h4" component="h2" id="credits" color="textPrimary">Credits</Typography>
+    <Typography variant="h2" component="h2" id="credits" color="textPrimary">the gimbap team</Typography>
     <Typography variant="body1" id="credits-body1" color="textPrimary">Miguel Hernandez</Typography>
     <Typography variant="body1" id="credits-body2" color="textPrimary">Angelynn Truong</Typography>
     <Typography variant="body1" id="credits-body3" color="textPrimary">Sebastien Fauque</Typography>
