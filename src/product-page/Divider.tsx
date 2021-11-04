14835fb2aae79e7eb287636d1c33b92119d051b9
parker9706 <pdhutcheson@gmail.com>
2021-11-04 09:34:58 -0700

    Committing on branch "Parker" for divider start

--- src/product-page/Divider.tsx
+++ src/product-page/Divider.tsx
@@ -5,6 +5,6 @@ export default function Divider() {
   return (
   <div className="dark">
  <Typography variant="h5" className="feature-header" id="feature-header-1">Cluster Reccomendations</Typography>
-        <Typography variant="body1" className="feature-body" id="feature-body-1">Provide detailed recomendations on where your application could benefit from a move to a microservice architecture.</Typography>
+   <Typography variant="body1" className="feature-body" id="feature-body-1">Provide detailed recomendations on where your application could benefit from a move to a microservice architecture.</Typography>
   </div> );
 }
