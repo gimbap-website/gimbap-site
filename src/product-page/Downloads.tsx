420653daac297ada0f21c04faca81bff1d074e5b
parker9706 <pdhutcheson@gmail.com>
2021-11-04 12:59:17 -0700

    Working on Downloads and Divider pages; Breaking for lecture

--- src/product-page/Downloads.tsx
+++ src/product-page/Downloads.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AppleIcon from '@mui/icons-material/Apple';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import WindowIcon from '@mui/icons-material/Window';
+import Link from '@material-ui/core/Link'; //need to fix
 
 
 export default function Downloads() {
   return (
   <div className="light">
-      <p>these are our downloads</p>
+    <Typography variant="h2" id="getting-started">Getting Started</Typography>
+    <Link href="http://www.github.com">
+    <GitHubIcon />
+    </Link>
+    <AppleIcon />
+    <WindowIcon />
   </div> );
 }
