dd667ab847b1828ad0e53d15de496517124238a6
parker9706 <pdhutcheson@gmail.com>
2021-11-04 18:29:45 -0700

    Added additional assets, removed unneeded ones, adjusted styling to show the banner on mobile devices.

--- src/product-page/Splash.tsx
+++ src/product-page/Splash.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import logo from './../../assets/gimbap-logo-green.png';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 
 
 export default function Splash() {
   return (
     <div className="dark">
-    <img id="main-logo" src={logo} />
+  <Grid
+  container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justifyContent="center"
+  style={{ minHeight: '100vh' }}
+>
+<img id="main-logo" src={logo} />
+  {/* <Grid item xs={3}> */}
+  </Grid>
+
     </div>
   )
 }
