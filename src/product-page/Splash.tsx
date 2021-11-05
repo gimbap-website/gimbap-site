e065a45c8a96d2e6a7b44186c0ca9b9bf80b3ced
angelynn <angelynn.trng@gmail.com>
2021-11-04 18:37:14 -0700

    Completed features section

--- src/product-page/Splash.tsx
+++ src/product-page/Splash.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import logo from './../../assets/gimbap-logo-green.png';
 import Box from '@mui/material/Box';
 
-
-
 export default function Splash() {
   return (
     <div className="dark">
