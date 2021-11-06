664063be4070d09448c4c45d5b6b50e261bb4446
parker9706 <pdhutcheson@gmail.com>
2021-11-06 14:51:36 -0700

    "Chevron added to navigate users to further components
    Co-authored-by: Angelynn Truong <angelynn.trng@gmail.com>"

--- src/product-page/Splash.tsx
+++ src/product-page/Splash.tsx
@@ -4,6 +4,9 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
 export default function Splash() {
+
+const executeScroll = () => document.getElementById('features').scrollIntoView()
+
   return (
     <div className="dark">
   <Grid
@@ -15,8 +18,15 @@ export default function Splash() {
   style={{ minHeight: '100vh' }}
 >
 <img id="main-logo" src={logo} />
-  {/* <Grid item xs={3}> */}
+
+<div id="chevron" onClick={executeScroll}>
+    <svg viewBox="0 0 68 33" className="chevron-down">
+      <path d="M.58 3.414l27.432 27.433c2.715 2.715 7.167 2.787 9.964.164L67.356 3.46 64.62.54 35.24 28.093c-1.222 1.146-3.212 1.114-4.4-.075L3.408.586.579 3.414z"></path>
+    </svg>
+</div>
+
   </Grid>
+
 
     </div>
   )
