311207b6def0b94bca1332344233c1b0c64ad9a2
angelynn <angelynn.trng@gmail.com>
2021-11-07 13:56:33 -0800

    Successfully set up demo routing

---
+++ src/demo/Demo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FeatureOne from './FeatureOne';
+
+
+export default function Demo(){
+
+  return (
+  <>
+      <FeatureOne />
+  </>);
+}
---
+++ src/demo/FeatureOne.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+export default function FeatureOne() {
+  return (<div className="dark section">
+    <h1>All the features you'll ever need</h1>
+  </div>);
+}
