23629a3c3f3bc6909c4b49c787798949f7f4416a
angelynn <angelynn.trng@gmail.com>
2021-11-07 15:16:34 -0800

    Completed demo page, added footer to documentation page

--- src/demo/Demo.tsx
+++ src/demo/Demo.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import FeatureOne from './FeatureOne';
-
+import FeatureLeft from './FeatureLeft';
+import FeatureRight from './FeatureRight';
+import Footer from './../product-page/Footer';
 
 export default function Demo(){
 
   return (
   <>
       <FeatureOne />
+      <FeatureRight />
+      <FeatureLeft />
+      <Footer />
   </>);
 }
