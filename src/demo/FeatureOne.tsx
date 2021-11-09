229280beb61164308c05244f3ddfe9220474c26d
parker9706 <pdhutcheson@gmail.com>
2021-11-09 15:22:27 -0700

    "Adjusted features for the demo page, and continuing to update to prepare for GIFs
    Co-authored-by: Angelynn Truong <angelynn.trng@gmail.com>"

--- src/demo/FeatureOne.tsx
+++ src/demo/FeatureOne.tsx
@@ -10,7 +10,7 @@ export default function FeatureOne() {
         <h1>All the features you'll ever need</h1>
         <Stack direction="row" spacing={12} alignItems="center" justifyContent="center">
         <div className="demoInfo">
-            <Typography variant="h4" className="demoFeature">This is a title</Typography>
+            <Typography variant="h4" className="demoFeature">Interactive Cluster Graph</Typography>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo
         </div>
         <div> <img src={featureOneImg}/> </div>
--- src/demo/FeatureRight.tsx
+++ src/demo/FeatureRight.tsx
@@ -9,7 +9,7 @@ export default function FeatureRight() {
     <Stack direction="row" spacing={12} alignItems="center" justifyContent="center">
       <div> <img src={featureOneImg}/> </div>
       <div className="demoInfo">
-      <Typography variant="h4" className="demoFeature">This is a title</Typography>
+      <Typography variant="h4" className="demoFeature">Individual Routes</Typography>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo
       </div>
     </Stack>
