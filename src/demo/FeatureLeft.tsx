25dd09545b00cf797185f4ae40e82377a94b7dde
parker9706 <pdhutcheson@gmail.com>
2021-11-10 22:50:25 -0700

    "Demo page updated with gifs
    Co-authored-by: Angelynn Truong <angelynn.trng@gmail.com>"

--- src/demo/FeatureLeft.tsx
+++ src/demo/FeatureLeft.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
-import featureOneImg from './../../assets/temporaryFeature1';
+import clusterDemo from './../../assets/cluster-demo.gif';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 export default function FeatureOne() {
-  return (<div className="dark section">
-    <Stack direction="row" spacing={12} alignItems="center" justifyContent="center">
+  return (
+  <div className="dark section">
+    <Stack direction="row" spacing={2} alignItems="center" justifyContent="center">
+
         <div className="demoInfo">
+
           <Typography variant="h4" className="demoFeature">Interactive Cluster Dendrogram</Typography>
+
           <div id="demo-string">
           Interact with your clustering recommendations to analyze the individual endpoints that have been grouped together based upon a similar covariant score.
           </div>
+
         </div>
-        <div> <img src={featureOneImg}/> </div>
+
+        <div>
+
+          <img src={clusterDemo} className="demoImg"/>
+
+        </div>
+
     </Stack>
   </div>);
 }
